b64d5e7df2fee54142e2f4fe91aa5f2c328ff2e7
sparent <sparent@adobe.com>
2013-09-09 14:00:39 -0700

    Fixing array serialization (broken with move to std::vector).

--- adobe/array.hpp
+++ adobe/array.hpp
@@ -15,6 +15,10 @@
 
 #include <adobe/any_regular.hpp>
 
+#ifdef ADOBE_STD_SERIALIZATION
+#include <adobe/iomanip.hpp>
+#endif
+
 /**************************************************************************************************/
 
 namespace adobe {
@@ -34,6 +38,25 @@ inline void push_back(array_t& v, any_regular_t x)
 } // namespace version_1
 
 using version_1::push_back;
+
+/**************************************************************************************************/
+
+#ifdef ADOBE_STD_SERIALIZATION
+
+inline std::ostream& operator<<(std::ostream& out, const array_t& x)
+{
+    out << begin_sequence;
+    
+    for (const auto& e : x) out << format(e);
+    
+    out << end_sequence;
+    
+    return out;
+}
+
+#endif
+
+/**************************************************************************************************/
 
 } // namespace adobe
 
