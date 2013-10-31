5b1902c86e9e3ff41bb8427d0a9532b7fb027f92
fbrereto <fosterb@gmail.com>
2013-10-31 10:09:17 -0700

    leveraging the boost multiprecision library to implement FNV-1a in 128, 256, 512, and 1024 flavors. Nifty.

--- adobe/fnv.hpp
+++ adobe/fnv.hpp
@@ -13,6 +13,10 @@
 
 #include <cstddef>
 
+#ifndef ADOBE_FNV_NO_BIGINTS
+#include <boost/multiprecision/cpp_int.hpp>
+#endif
+
 /*************************************************************************************************/
 /**
     Implementation of the FNV (Fowler Noll Vo) class of hash algorithms. From
@@ -62,23 +66,115 @@ struct fnv_traits
 /*************************************************************************************************/
 
 template <>
-struct fnv_traits<64>
+struct fnv_traits<32>
 {
-    typedef std::uint64_t                                          value_type;
-    typedef std::integral_constant<value_type, 0xcbf29ce484222325> offset_basis_k;
-    typedef std::integral_constant<value_type, 0x100000001b3>      prime_k;
+    typedef std::uint32_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0x811c9dc5;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x1000193;
+    }
 };
 
 /*************************************************************************************************/
 
 template <>
-struct fnv_traits<32>
+struct fnv_traits<64>
 {
-    typedef std::uint32_t                                  value_type;
-    typedef std::integral_constant<value_type, 0x811c9dc5> offset_basis_k;
-    typedef std::integral_constant<value_type, 0x1000193>  prime_k;
+    typedef std::uint64_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0xcbf29ce484222325;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x100000001b3;
+    }
 };
 
+/*************************************************************************************************/
+#ifndef ADOBE_FNV_NO_BIGINTS
+using namespace boost::multiprecision::literals;
+
+/*************************************************************************************************/
+
+template <>
+struct fnv_traits<128>
+{
+    typedef boost::multiprecision::uint128_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0x6C62272E07BB014262B821756295C58D_cppui128;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x1000000000000000000013B_cppui128;
+    }
+};
+
+/*************************************************************************************************/
+
+template <>
+struct fnv_traits<256>
+{
+    typedef boost::multiprecision::uint256_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0xDD268DBCAAC550362D98C384C4E576CCC8B1536847B6BBB31023B4C8CAEE0535_cppui256;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x1000000000000000000000000000000000000000163_cppui256;
+    }
+};
+
+/*************************************************************************************************/
+
+template <>
+struct fnv_traits<512>
+{
+    typedef boost::multiprecision::uint512_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0xB86DB0B1171F4416DCA1E50F309990ACAC87D059C90000000000000000000D21E948F68A34C192F62EA79BC942DBE7CE182036415F56E34BAC982AAC4AFE9FD9_cppui512;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x100000000000000000000000000000000000000000000000000000000000000000000000000000000000157_cppui512;
+    }
+};
+
+/*************************************************************************************************/
+
+template <>
+struct fnv_traits<1024>
+{
+    typedef boost::multiprecision::uint1024_t value_type;
+
+    static constexpr value_type offset_basis()
+    {
+        return 0x5F7A76758ECC4D32E56D5A591028B74B29FC4223FDADA16C3BF34EDA3674DA9A21D9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004C6D7EB6E73802734510A555F256CC005AE556BDE8CC9C6A93B21AFF4B16C71EE90B3_cppui1024;
+    }
+
+    static constexpr value_type prime()
+    {
+        return 0x10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018D_cppui1024;
+    }
+};
+#endif
 /*************************************************************************************************/
 
 } // namespace detail
@@ -103,11 +199,11 @@ fnvtype<BitCount> fnv1a(Iterator first, Iterator last)
 
     typedef fnvtype<BitCount> result_type;
 
-    result_type result(detail::fnv_traits<BitCount>::offset_basis_k::value);
+    result_type result(detail::fnv_traits<BitCount>::offset_basis());
 
     while (first != last)
         result = (result xor static_cast<result_type>(*first++)) *
-                     detail::fnv_traits<BitCount>::prime_k::value;
+                     detail::fnv_traits<BitCount>::prime();
 
     return result;
 }
