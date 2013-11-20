d4ea66b2e36d71729812f5c0a1daef18e5b7133e
Foster Brereton <fbrereto@adobe.com>
2013-11-19 20:45:27 -0800

    more clang formatting

--- clang_format.sh
+++ clang_format.sh
@@ -6,4 +6,4 @@
 
 cd `dirname $0`
 
-find . -name '*.[c|h]pp' -print | xargs clang-format -i -style=file
+find . -name '*.[c|h]pp' \! -name "shavs.cpp" -print | xargs -n 1 clang-format -i -style=file
